782adb029b44cf8d15387beccb1bf190c1247183
Cyrill Martin <cyrill.martin@kmapper.com>
2026-03-06 14:17:46 +0100

    Update useScreen.j

--- __frontend/app/composables/useScreen.js
+++ __frontend/app/composables/useScreen.js
@@ -2,8 +2,8 @@ const mobileThreshold = 768;
 const tabletThreshold = 1024;
 
 export const useScreen = () => {
-  const width = useState("screen-width", () => 0);
-  const height = useState("screen-height", () => 0);
+  const width = useState("screen-width", () => 1280);
+  const height = useState("screen-height", () => 800);
 
   const isMobile = computed(() => width.value <= mobileThreshold);
   const isTablet = computed(() => width.value > mobileThreshold && width.value <= tabletThreshold);
